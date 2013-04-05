8ccf984a90bd5d7a521f869dd285203d303623a0
Victor Boivie <victor@boivie.com>
2013-04-05 15:29:32 +0200

    Added maintenance schedules

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def before_request():
 @app.route("/s/<sid>", methods = ["GET", "POST"])
 def update(sid):
     now = int(time.time())
-    old_lbls = g.db.hget("lb:s:%s" % sid, "lbls") or ""
+    old_lbls, old_maint = g.db.hmget("lb:s:%s" % sid, "lbls", "maint")
+    old_lbls = old_lbls or ""
     old_lbls = set([l for l in old_lbls.split(",") if l])
     new_lbls = set([])
     if request.form.get('labels'):
@@ -44,8 +45,10 @@ def update(sid):
         elif key == 'warning':
             conds.append('w:%s' % value)
     cond = ';'.join(conds)
+    new_maint = request.form.get('maint')
     with g.db.pipeline() as pipe:
         pipe.sadd("lb:services:all", sid)
+        # Remove 'soft maintentance' if we get a good status
         for lbl in new_lbls - old_lbls:
             pipe.sadd("lb:services:%s" % lbl, sid)
             pipe.sadd("lb:labels", lbl)
@@ -53,12 +56,22 @@ def update(sid):
             pipe.srem("lb:services:%s" % lbl, sid)
         if old_lbls != new_lbls:
             pipe.hset("lb:s:%s" % sid, "lbls", ",".join(sorted(new_lbls)))
-        pipe.lpush("lb:s:%s:h" % sid, '%d:1' % now)
-        pipe.ltrim("lb:s:%s:h" % sid, 0, MAX_SAVED - 1)
-        pipe.hset("lb:s:%s" % sid, "lval", '1')
-        pipe.hset("lb:s:%s" % sid, "ts", now)
-        if conds:
-            pipe.hset("lb:s:%s" % sid, "cond", cond)
+
+        if new_maint:
+            maint_type, expiry = new_maint.split(":")
+            expiry = int(expiry) + now
+            pipe.hset("lb:s:%s" % sid, "maint", "%s:%d" % (maint_type, expiry))
+        else:
+            if old_maint:
+                maint_type, expiry = old_maint.split(":")
+                if maint_type == 'soft':
+                    pipe.hset("lb:s:%s" % sid, "maint", "")
+            pipe.lpush("lb:s:%s:h" % sid, '%d:1' % now)
+            pipe.ltrim("lb:s:%s:h" % sid, 0, MAX_SAVED - 1)
+            pipe.hset("lb:s:%s" % sid, "lval", '1')
+            pipe.hset("lb:s:%s" % sid, "ts", now)
+            if conds:
+                pipe.hset("lb:s:%s" % sid, "cond", cond)
         pipe.execute()
     return "ok"
 
@@ -86,17 +99,21 @@ def eval_conds(conds, lval, ts, now):
 
 def get_services(lbl):
     now = int(time.time())
-    fields = ("#", "lb:s:*->lval", "lb:s:*->ts", "lb:s:*->cond")
+    fields = ("#", "lb:s:*->lval", "lb:s:*->ts", "lb:s:*->cond", "lb:s:*->maint")
     services = []
-    for sid, lval, ts, cond in chunks(g.db.sort("lb:services:%s" % lbl, get=fields), 4):
+    for sid, lval, ts, cond, maint in \
+            chunks(g.db.sort("lb:services:%s" % lbl, get=fields), 5):
         conds = parse_conds(cond or DEFAULT_COND)
         warnings, errors = eval_conds(conds, lval, int(ts), now)
-        if len(warnings) == 0 and len(errors) == 0:
-            status = 'ok'
-        elif len(errors) == 0:
-            status = 'warning'
-        else:
+        status = 'ok'
+        if maint:
+            maint_type, maint_expiry = maint.split(":")
+            if int(maint_expiry) >= now:
+                status = 'maint'
+        if status != 'maint' and len(errors) > 0:
             status = 'error'
+        elif status != 'maint' and len(warnings) > 0:
+            status = 'warning'
         services.append({'sid': sid, 'ts': ts, 'status': status,
                          'warnings': warnings, 'errors': errors})
     return services
@@ -123,9 +140,11 @@ def get_list_raw(lbl):
     services = get_services(lbl)
     has_warnings = len([s for s in services if s['status'] == 'warning']) > 0
     has_errors = len([s for s in services if s['status'] == 'error']) > 0
+    has_maint = len([s for s in services if s['status'] == 'maint']) > 0
     html = render_template("dashboard.html", services=services,
                            has_warnings=has_warnings,
-                           has_errors=has_errors)
+                           has_errors=has_errors,
+                           has_maint=has_maint)
     resp = make_response(html)
     resp.headers["Content-type"] = "text/plain"
     return resp
